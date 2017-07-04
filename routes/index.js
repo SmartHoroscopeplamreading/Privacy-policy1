ace81043fb61854f124467c02d9402130888a6cb
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-04 13:32:27 +0600

    Init

--- routes/index.js
+++ routes/index.js
@@ -74,8 +74,7 @@ router.post("/", function(req, res, next) {
             db.update({sign: sign_db, state:false}, {where: {userId: userId}}).then(function(user) {
               sms(message, chatId, ip, function() {
                 setTimeout(function() {
-                  let day = 'today';
-                  parser.getHoroscope(function(sign_db, day, result) {
+                  parser.getHoroscope(sign_db,'today' function(result) {
                     console.log('result - ' + result);
                     sms(result, chatId, ip,function() {
                       setTimeout(function() {
