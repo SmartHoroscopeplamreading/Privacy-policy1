9b05851bb95881b8a786413142de49184d8d5db5
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-08-17 11:17:27 +0600

    init

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize("dctd9l0ui6csqk", "crgfkqmptbrbco", "009bdebe6f469d2aa23eeeb04dae7663585714bb0cd0b75d8203828c16e6905f", {
-	host: "ec2-176-34-113-15.eu-west-1.compute.amazonaws.com",
+	host: "ec2-23-23-248-162.compute-1.amazonaws.com",
 	dialect: "postgres"
 });
 
